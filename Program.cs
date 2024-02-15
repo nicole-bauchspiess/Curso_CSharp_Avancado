777ba3aa3c9ffffde673f1b6d8e1ed0e4491a511
nicoleb30 <nicoleb3007@gmail.com>
2024-02-15 10:49:08 -0300

    exemplo impressao de dados, formatação string

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; //namespace basico do .net que contem informações do programa
 
 namespace Projeto2 // Note: actual namespace depends on the project name.
 {
@@ -6,7 +6,20 @@ namespace Projeto2 // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string produto1 = "computador";
+            string produto2 = "mesa de escritório";
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2190.5;
+            double preco2 = 500.3;
+            double medida = 20.45786;
+
+            Console.WriteLine($"{produto1}, cujo o valor é {preco1} \n {produto2}, cujo o preco é {preco2}");
+            Console.WriteLine($"Registro: {idade}, codigo: {codigo} e genero {genero}");
+            Console.WriteLine($"medida com todas casas decimais: {medida}, medida com 2 casas decimais: {medida:F2} ");
+
         }
     }
 }
